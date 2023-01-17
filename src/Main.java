6fb2253362a56804e0a797b1bc75bb23811f1681
Luciana Ferreira da Silva <luciana.silva.1993@gmail.com>
2023-01-17 20:45:24 -0300

    Iniciando!

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bem vindo ao Hospital Nada Mal, Aqui você sai legal!");
+        System.out.println("Por favor, escolha uma das opções abaixo:");
+    }
+}
